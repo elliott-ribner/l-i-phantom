986f892542e77b5963e110592ea84e4bdd48a1e9
elliott-ribner <elr886@gmail.com>
2015-07-10 12:37:48 -0400

    writing

--- scrape_test.js
+++ scrape_test.js
@@ -1,29 +1,42 @@
+var fs = require("fs");
+var schools = require('./sample.json');
 var webPage = require('webpage');
 var page = webPage.create();
 var similarSchoolBlock;
+var outputArray = [];
 
-var schoolArray = 
-{
-  "1200": {
-    "url": "https://www.linkedin.com/edu/nyack-college-18998", 
-    "citystate": "Nyack, NY", 
-    "name": "Nyack College"
-  }, 
-  "344": {
-    "url": "https://www.linkedin.com/edu/concordia-university-wisconsin-19670", 
-    "citystate": "Mequon, WI", 
-    "name": "Concordia University Wisconsin"
-  }
+var schoolArray = JSON.parse(JSON.stringify(schools));
+console.log(JSON.stringify(schoolArray));
+
+for(var prop in schoolArray){
+  var school = JSON.parse(JSON.stringify(schoolArray[prop]));
+  console.log(school.url);
+  logPage(school.name, school.url);
 };
 
-console.log(schoolArray)
-// page.open('https://www.linkedin.com/edu/school?id=18753&trk=tyah&trkInfo=clickedVertical%3Aschool%2Cidx%3A3-1-9%2CtarId%3A1436230271851%2Ctas%3Amontana%20state', function(status){
-	
-// 	similarSchoolBlock = page.evaluate(function(){
-// 		 return document.querySelector('.similar-schools').innerHTML;
 
-// 	});
-	
-// 	console.log(similarSchoolBlock);
-// 	phantom.exit();
-// });	
+
+function logPage(schoolName, schoolUrl ) {
+  page.open('https://www.linkedin.com/edu/school?id=18753&trk=tyah&trkInfo=clickedVertical%3Aschool%2Cidx%3A3-1-9%2CtarId%3A1436230271851%2Ctas%3Amontana%20state', function(status){
+  	similarSchoolBlock = page.evaluate(function(){
+  		return document.querySelector('.similar-schools').innerHTML;
+  	});
+  	
+  	var tempJson = {
+      "name": schoolName,
+      "linked_in_html": similarSchoolBlock
+    };
+    outputArray.push(tempJson);
+    //needs to be moved outside of this method
+    var path = 'school_html_block.json';
+    fs.write( path, JSON.stringify(tempJson, undefined, 2), function (err) {
+      if (err) throw err;
+      console.log('It\'s saved!');
+    });
+    //
+  	phantom.exit();
+  });	
+
+};
+
+
